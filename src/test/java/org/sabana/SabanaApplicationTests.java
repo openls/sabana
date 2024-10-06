775c2facc8a32dfbd658dc63ae0e36d5e03f3160
yamelsenih <ysenih@erpya.com>
2024-10-06 12:43:47 -0400

    Adda test for version

--- src/test/java/org/sabana/SabanaApplicationTests.java
+++ src/test/java/org/sabana/SabanaApplicationTests.java
@@ -1,13 +1,29 @@
 package org.sabana;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.sabana.core.entity.ApplicationVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SabanaApplicationTests {
 
+	@Autowired
+	private TestRestTemplate tester;
+
 	@Test
-	void contextLoads() {
+	void version() {
+		ResponseEntity<ApplicationVersion> versionEntity = this.tester.getForEntity("/version", ApplicationVersion.class);
+		ApplicationVersion version = versionEntity.getBody();
+		assertNotNull(version);
+		assertTrue(version.getName().contains("Sabana"));
 	}
 
 }
