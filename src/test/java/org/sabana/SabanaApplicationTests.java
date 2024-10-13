7e9f014827eb8ef6b28bf4c3d52b5d3d58b44169
yamelsenih <ysenih@erpya.com>
2024-10-13 03:32:47 -0400

    minor change

--- src/test/java/org/sabana/SabanaApplicationTests.java
+++ src/test/java/org/sabana/SabanaApplicationTests.java
@@ -1,6 +1,5 @@
 package org.sabana;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
